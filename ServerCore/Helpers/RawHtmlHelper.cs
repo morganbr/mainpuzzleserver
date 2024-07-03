cf23948c25d68c4501019de1d387341e59ec868a
Morgan Brown <atrader42@hotmail.com>
2024-07-03 05:48:51 -0700

    Adds a blazor component for the puzzle list to allow it to embed an unlimited number of presence components. Also simplifies the layout based on embedded puzzles and should allow snappier sorting and filtering

--- ServerCore/Helpers/RawHtmlHelper.cs
+++ ServerCore/Helpers/RawHtmlHelper.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Html;
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Html;
 using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace ServerCore.Helpers
@@ -6,7 +7,7 @@ namespace ServerCore.Helpers
     public static class RawHtmlHelper
     {
         /// <summary>
-        /// Helper for extracting raw HTML from a string if present.
+        /// Helper for extracting raw HTML for Razor Pages from a string if present.
         /// The syntax we look for is: {anything}Html.Raw({raw html})
         /// If we see that syntax, we return the {raw html} part only.
         /// If we see anything else, we return the entire string, processed by the standard ASP.NET IHtmlHelper.
@@ -15,18 +16,51 @@ namespace ServerCore.Helpers
         /// <returns>IHtmlContent containing either the raw html processed by Html.Raw or the entire string processed by Html.DisplayFor</returns>
         public static IHtmlContent Display<T>(string text, int eventId, IHtmlHelper<T> helper)
         {
-            if (text != null && text.EndsWith(")") && text.Contains("Html.Raw("))
+            // Note that because this returns an ASP object, this can't be used in Javascript on pages that have already loaded
+            // For example, Pages\Submissions\Index.cshtml duplicates this stripping of "Html.Raw(" in Javascript since it can't call this
+            string asRawHtml = GetRawHtml(text, eventId);
+            if (asRawHtml != null)
             {
-                // Note that because this returns an ASP object, this can't be used in Javascript on pages that have already loaded
-                // For example, Pages\Submissions\Index.cshtml duplicates this stripping of "Html.Raw(" in Javascript since it can't call this
-                text = text.Replace("{eventId}", $"{eventId}");
-                int start = text.IndexOf("Html.Raw(") + 9;
-                return helper.Raw(text.Substring(start, text.Length - start - 1));
+                return helper.Raw(asRawHtml);
             }
             else
             {
                 return helper.DisplayFor(m => text);
             }
+        }
+
+        /// <summary>
+        /// Helper for extracting raw HTML for Blazor from a string if present.
+        /// The syntax we look for is: {anything}Html.Raw({raw html})
+        /// If we see that syntax, we return the {raw html} part only.
+        /// If we see anything else, we return the entire string, processed by the standard ASP.NET IHtmlHelper.
+        /// </summary>
+        /// <param name="text">text which might contain raw HTML in</param>
+        /// <returns>MarkupString containing the raw html processed by Html.Raw or the entire string</returns>
+
+        public static object GetMarkupString(string text, int eventId)
+        {
+            string asRawHtml = GetRawHtml(text, eventId);
+            if (asRawHtml != null)
+            {
+                return new MarkupString(asRawHtml);
+            }
+            else
+            {
+                return text;
+            }
+        }
+
+        private static string? GetRawHtml(string text, int eventId) 
+        {
+            if (text != null && text.EndsWith(")") && text.Contains("Html.Raw("))
+            {
+                text = text.Replace("{eventId}", $"{eventId}");
+                int start = text.IndexOf("Html.Raw(") + 9;
+                return text.Substring(start, text.Length - start - 1);
+            }
+
+            return null;
         }
     }
 }
---
+++ ServerCore/Pages/Puzzles/PuzzleList.razor.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+using Microsoft.EntityFrameworkCore;
+using ServerCore.DataModel;
+using ServerCore.ModelBases;
+
+namespace ServerCore.Pages.Puzzles
+{
+    public partial class PuzzleList
+    {
+        [Parameter]
+        public int PuzzleUserId { get; set; }
+
+        [Parameter]
+        public int TeamId { get; set; }
+
+        [Parameter]
+        public int EventId { get; set; }
+
+        [Parameter]
+        public EventRole EventRole { get; set; }
+
+        [Inject]
+        public PuzzleServerContext _context { get; set; }
+
+        Event Event { get; set; }
+
+        Team Team { get; set; }
+
+        IEnumerable<PuzzleView> VisibleTeamPuzzleViews { get; set; } = Enumerable.Empty<PuzzleView>();
+
+        bool Loaded = false;
+
+        public class PuzzleView
+        {
+            public int ID { get; set; }
+            public string Group { get; set; }
+            public int OrderInGroup { get; set; }
+            public string Name { get; set; }
+            public string Errata { get; set; }
+            //public string PuzzleUrl { get; set; }
+            //public string SolutionUrl { get; set; }
+            public DateTime? SolvedTime { get; set; }
+            public PieceMetaUsage PieceMetaUsage { get; set; }
+            //public string CustomUrl { get; set; }
+            //public string CustomSolutionUrl { get; set; }
+            public string ViewUrl { get; set; }
+        }
+
+        public enum SortOrder
+        {
+            PuzzleAscending,
+            PuzzleDescending,
+            GroupAscending,
+            GroupDescending,
+            SolveAscending,
+            SolveDescending
+        }
+
+        public enum PuzzleStateFilter
+        {
+            All,
+            Unsolved
+        }
+
+        SortOrder _sortOrder { get; set; }
+
+        const SortOrder DefaultSort = SortOrder.GroupAscending;
+
+        PuzzleStateFilter _stateFilter { get; set; }
+
+        protected override async Task OnParametersSetAsync()
+        {
+            Event = await _context.Events.Where(ev => ev.ID == EventId).SingleAsync();
+            Team = await _context.Teams.Where(t => t.ID == TeamId).SingleAsync();
+
+            VisibleTeamPuzzleViews = await GetVisibleTeamPlayerPuzzleViews(_sortOrder);
+
+            Loaded = true;
+            await base.OnParametersSetAsync();
+        }
+
+        private async Task<IEnumerable<PuzzleView>> GetVisibleTeamPlayerPuzzleViews(SortOrder? sortOrder)
+        {
+            // If the event has not yet begun, no puzzles yet
+            if (DateTime.UtcNow < Event.EventBegin && !Team.IsDisqualified)
+            {
+                return Enumerable.Empty<PuzzleView>();
+            }
+
+            // all puzzles for this event that are real puzzles
+            var puzzlesInEventQ = _context.Puzzles.Where(puzzle => puzzle.Event.ID == Event.ID && puzzle.IsPuzzle && !puzzle.IsForSinglePlayer);
+
+            // unless we're in a global lockout, then filter to those!
+            var puzzlesCausingGlobalLockoutQ = PuzzleStateHelper.PuzzlesCausingGlobalLockout(_context, Event, Team);
+            if (await puzzlesCausingGlobalLockoutQ.AnyAsync())
+            {
+                puzzlesInEventQ = puzzlesCausingGlobalLockoutQ;
+            }
+
+            // all puzzle states for this team that are unlocked (note: IsUnlocked bool is going to harm perf, just null check the time here)
+            // Note that it's OK if some puzzles do not yet have a state record; those puzzles are clearly still locked and hence invisible.
+            // All puzzles will show if all answers have been released)
+            var stateForTeamQ = _context.PuzzleStatePerTeam.Where(state => state.TeamID == Team.ID && (Event.AreAnswersAvailableNow || state.UnlockedTime != null));
+
+            // join 'em (note: just getting all properties for max flexibility, can pick and choose columns for perf later)
+            // Note: EF gotcha is that you have to join into anonymous types in order to not lose valuable stuff
+            var visiblePuzzlesQ = from Puzzle puzzle in puzzlesInEventQ
+                                  join PuzzleStatePerTeam pspt in stateForTeamQ on puzzle.ID equals pspt.PuzzleID
+
+                                  select new PuzzleView { ID = puzzle.ID, Group = puzzle.Group, OrderInGroup = puzzle.OrderInGroup, Name = puzzle.Name,/* CustomUrl = puzzle.CustomURL, CustomSolutionUrl = puzzle.CustomSolutionURL,*/ Errata = puzzle.Errata, SolvedTime = pspt.SolvedTime, PieceMetaUsage = puzzle.PieceMetaUsage,
+                                  ViewUrl = $"/{Event.EventID}/{EventRole}/Submissions/{puzzle.ID}"};
+
+            // todo morganb: sort and filter should be separate from the db query
+            if (_stateFilter == PuzzleStateFilter.Unsolved)
+            {
+                visiblePuzzlesQ = visiblePuzzlesQ.Where(puzzles => puzzles.SolvedTime == null);
+            }
+
+            return GetSortedView(visiblePuzzlesQ, sortOrder);
+        }
+
+        private IEnumerable<PuzzleView> GetSortedView(IEnumerable<PuzzleView> puzzleViews, SortOrder? sortOrder)
+        {
+            SortOrder actualSortOrder = sortOrder.HasValue ? sortOrder.Value : DefaultSort;
+
+            switch (actualSortOrder)
+            {
+                case SortOrder.PuzzleAscending:
+                    return puzzleViews.OrderBy(pv => pv.Name);
+                case SortOrder.PuzzleDescending:
+                    return puzzleViews.OrderByDescending(pv => pv.Name);
+                case SortOrder.GroupAscending:
+                    return puzzleViews.OrderBy(pv => pv.Group).ThenBy(pv => pv.OrderInGroup).ThenBy(pv => pv.Name);
+                case SortOrder.GroupDescending:
+                    return puzzleViews.OrderByDescending(pv => pv.Group).ThenByDescending(pv => pv.OrderInGroup).ThenByDescending(pv => pv.Name);
+                case SortOrder.SolveAscending:
+                    return puzzleViews.OrderBy(pv => pv.SolvedTime ?? DateTime.MaxValue);
+                case SortOrder.SolveDescending:
+                    return puzzleViews.OrderByDescending(pv => pv.SolvedTime ?? DateTime.MaxValue);
+                default:
+                    throw new ArgumentException($"unknown sort: {sortOrder}");
+            }
+        }
+    }
+}
